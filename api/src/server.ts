3efd77fd2a0999f29c3264664ebd6e9745a4b21a
Jamerson Aguiar <jamersonaguiar@alu.ufc.br>
2020-06-02 14:07:15 -0300

    Added CORS module

--- api/src/server.ts
+++ api/src/server.ts
@@ -5,8 +5,8 @@ import routes from './routes';
 
 const app = express();
 
-app.use(cors);
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
 app.use('/uploads', express.static(resolve(__dirname, '..', 'uploads')));
