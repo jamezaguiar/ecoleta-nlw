c58e2a93b0a2e265219886c2e70225cceb0ef215
Jamerson Aguiar <jamersonaguiar@alu.ufc.br>
2020-06-06 14:28:20 -0300

    Image upload finalized

--- web/src/components/Dropzone/index.tsx
+++ web/src/components/Dropzone/index.tsx
@@ -1,21 +1,47 @@
-import React, { useCallback } from "react";
+import React, { useState, useCallback } from "react";
 import { useDropzone } from "react-dropzone";
+import { FiUpload } from "react-icons/fi";
 
 import "./styles.css";
 
-const Dropzone = () => {
-  const onDrop = useCallback((acceptedFiles) => {
-    // Do something with the files
-  }, []);
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+interface Properties {
+  onFileUploaded: (file: File) => void;
+}
+
+const Dropzone: React.FC<Properties> = ({ onFileUploaded }) => {
+  const [selectedImageURL, setSelectedImageURL] = useState("");
+
+  const onDrop = useCallback(
+    (acceptedFiles) => {
+      const file = acceptedFiles[0];
+      const fileURL = URL.createObjectURL(file);
+
+      setSelectedImageURL(fileURL);
+      onFileUploaded(file);
+    },
+    [onFileUploaded]
+  );
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+    onDrop,
+    accept: "image/*",
+  });
 
   return (
-    <div {...getRootProps()}>
-      <input {...getInputProps()} />
-      {isDragActive ? (
-        <p>Drop the files here ...</p>
+    <div className="dropzone" {...getRootProps()}>
+      <input {...getInputProps()} accept="image/*" />
+
+      {selectedImageURL ? (
+        <img src={selectedImageURL} alt="Imagem do ponto de coleta" />
+      ) : isDragActive ? (
+        <p>
+          <FiUpload />
+          Solte o arquivo
+        </p>
       ) : (
-        <p>Drag 'n' drop some files here, or click to select files</p>
+        <p>
+          <FiUpload />
+          Arraste ou clique para adicionar a imagem do seu ponto de coleta
+        </p>
       )}
     </div>
   );
--- web/src/pages/CreatePoint/index.tsx
+++ web/src/pages/CreatePoint/index.tsx
@@ -3,6 +3,7 @@ import { Link, useHistory } from "react-router-dom";
 import { FiArrowLeft } from "react-icons/fi";
 import { LeafletMouseEvent } from "leaflet";
 import { Map, TileLayer, Marker } from "react-leaflet";
+import Dropzone from "../../components/Dropzone";
 import axios from "axios";
 
 import api from "../../services/api";
@@ -48,6 +49,7 @@ const CreatePoint = () => {
     0,
     0,
   ]);
+  const [selectedImage, setSelectedImage] = useState<File>();
 
   const history = useHistory();
 
@@ -134,16 +136,17 @@ const CreatePoint = () => {
     const [latitude, longitude] = selectedPosition;
     const items = selectedItems;
 
-    const data = {
-      name,
-      email,
-      whatsapp,
-      uf,
-      city,
-      latitude,
-      longitude,
-      items,
-    };
+    const data = new FormData();
+
+    data.append("name", name);
+    data.append("email", email);
+    data.append("whatsapp", whatsapp);
+    data.append("uf", uf);
+    data.append("city", city);
+    data.append("latitude", String(latitude));
+    data.append("longitude", String(longitude));
+    data.append("items", items.join(","));
+    if (selectedImage) data.append("image", selectedImage);
 
     await api.post("points", data);
 
@@ -169,6 +172,8 @@ const CreatePoint = () => {
           <br />
           ponto de coleta
         </h1>
+
+        <Dropzone onFileUploaded={setSelectedImage} />
 
         <fieldset>
           <legend>
